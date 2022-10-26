8df89cdef7f0543550ba96eb5cbd56aa958f0249
sawroar <sawroarhossain5@gmail.com>
2022-10-26 13:29:27 +0600

    course category Added

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const course = require('./courses.json')
 app.get('/', (req, res) => {
     res.send('Course API Runing');
+})
+app.get('/courses', (req, res) => {
+    res.send(course)
 })
 app.listen(port, () => {
     console.log('Course Api server on port', port)
